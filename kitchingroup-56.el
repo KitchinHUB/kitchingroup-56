8201a3f459635bbdd641322153eae6a4c2dc669e
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-01 08:42:07 -0500

    init

---
+++ kitchingroup-56.el
@@ -0,0 +1,37 @@
+;;; kitchingroup-56.el --- CaPPA preprint for doi:
+
+;; Copyright (C) 2016 John Kitchin
+
+;; Version: 0.0.1
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; DOI: 10.1016/j.susc.2015.05.007
+;; Journal: Surface Science
+;; Bibtex: @article{kitchin-2015-data-surfac-scien,
+;;   author =	 "John R. Kitchin",
+;;   title =	 {Data Sharing in Surface Science},
+;;   journal =	 "Surface Science ",
+;;   volume =	 "N/A",
+;;   pages =	 "in press",
+;;   year =	 2015,
+;;   doi =		 {10.1016/j.susc.2015.05.007},
+;;   url =
+;;                   "http://www.sciencedirect.com/science/article/pii/S0039602815001326",
+;;   issn =	 "0039-6028",
+;;   keywords =	 {DESC0004031, early-career, orgmode, Data sharing },
+;; }
+
+;; TBD
+
+;;; Commentary:
+
+;; Put some descriptive text here.
+
+;;; Code:
+(require 'cappa)
+
+(cappa-register 'kitchingroup-56)
+
+
+(provide 'kitchingroup-56)
+;;; kitchingroup-56.el ends here
